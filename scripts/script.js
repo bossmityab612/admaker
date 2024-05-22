cac9c56ea05099cf516e6b291563ce6dbddd4b21
Anastasia Bashkirova <bossmityab612@gmail.com>
2024-05-22 11:37:07 +0300

    style: adaptive mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,31 @@
-const swiper = new Swiper('.growth__cards-block-main', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 3,
-
-
-  // If we need pagination
-  pagination: {
-    el: '.growth__cards-block-main .swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.growth__cards-block-main .swiper-button-next',
-    prevEl: '.growth__cards-block-main .swiper-button-prev',
-  },
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true
+  });
 });
+
+// const swiper = new Swiper('.growth__cards-block-main', {
+//   // Optional parameters
+//   loop: true,
+//   slidesPerView: 3,
+
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.growth__cards-block-main .swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.growth__cards-block-main .swiper-button-next',
+//     prevEl: '.growth__cards-block-main .swiper-button-prev',
+//   },
+// });
 
 // const tariffsSwiper = new Swiper('.tariffs__slider', {
 //   // Optional parameters
