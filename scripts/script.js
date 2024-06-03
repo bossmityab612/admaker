e02553f46dfc84fcb2ff857e07417a4b2dec798f
Anastasia Bashkirova <bossmityab612@gmail.com>
2024-06-03 06:31:35 +0300

    style: footer adaptive

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,28 @@
-const swiper = new Swiper('.growth__cards-block-main', {
-    // Optional parameters
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 1,
     loop: true,
-    slidesPerView: 3,
-  
-    // If we need pagination
-    pagination: {
-      el: '.growth__cards-block-main .swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.growth__cards-block-main .swiper-button-next',
-      prevEl: '.growth__cards-block-main .swiper-button-prev',
-    },
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true
   });
+});
+
+// const swiper = new Swiper('.growth__cards-block-main', {
+//   // Optional parameters
+//   loop: true,
+//   slidesPerView: 3,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.growth__cards-block-main .swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.growth__cards-block-main .swiper-button-next',
+//     prevEl: '.growth__cards-block-main .swiper-button-prev',
+//   },
+// });
 
