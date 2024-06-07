66c62ac827c82777b9e3c59d2d2ba66938c6ebaa
Anastasia Bashkirova <bossmityab612@gmail.com>
2024-06-07 17:11:18 +0300

    style: popup-window create

--- scripts/script.js
+++ scripts/script.js
@@ -9,94 +9,35 @@ $(document).ready(function(){
   });
 });
 
-const swiper = new Swiper('.intro__main-block', {
+const swiper = new swiper('.intro__block-slider', {
   // Optional parameters
   loop: true,
 
   // If we need pagination
-  pagination: {
-    el: '.intro__main-block .swiper-pagination',
-  },
+  // pagination: {
+  //   el: '.intro__block-slider .swiper-pagination',
+  // },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.intro__main-block .swiper-button-next',
-    prevEl: '.intro__main-block .swiper-button-prev',
+    nextEl: '.intro__block-slider .swiper-button-next',
+    prevEl: '.intro__block-slider .swiper-button-prev',
   },
 });
 
-const tariffsSwiper = new Swiper('.tariffs__slider', {
-  // Optional parameters
-  // loop: true,
-  slidesPerView: 1,
 
-  // If we need pagination
-  pagination: {
-    el: '.tariffs__slider .swiper-pagination',
-  },
+const popup = document.querySelector('.popup'); //Всплывающее окно
+const openPopupButton = document.querySelector('.open-popup'); //Кнопка открытия попапа
+const closePopupButton = document.querySelector('.popup__button-close'); //Кнопка закрытия попапа
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.tariffs__slider .swiper-button-next',
-    prevEl: '.tariffs__slider .swiper-button-prev',
-  },
+// Открытие и закрытие попапа
+openPopupButton.addEventListener('click', () => {
+	popup.classList.add('popup_opened');
+	nameForm.value = nameInput.textContent;
+	jobForm.value = jobInput.textContent;
 });
 
-function sliderAnimate(index) {
-  let buttonsCount = $('.levels__list-item').length - 1;
-  $('.levels__line-front').animate({
-    width: 100 / buttonsCount * index + "%"
-  });
-}
-
-
-const levelSwiper = new Swiper('.levels__main-block', {
-  // Optional parameters
-  spaceBetween: 300,
-
-  // If we need pagination
-  pagination: {
-    el: '.levels__main-block .swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.levels__main-block .swiper-button-next',
-    prevEl: '.levels__main-block .swiper-button-prev',
-  }
+closePopupButton.addEventListener('click', () => {
+  popup.classList.remove('popup_opened');
 });
 
-levelSwiper.on('slideChangeTransitionStart', function() {
-  $('.levels__description-block').each(function(index){
-    if ($('.levels__description-block').eq(index).hasClass('swiper-slide-active')) {
-      $('.levels__list-item').removeClass('active')
-      $('.levels__list-item').eq(index).addClass('active')
-      sliderAnimate(index)
-    }
-  })
-});
-
-$('.button-animated').hover(function() {
-  $(this).find('.button-animated-image').fadeToggle(300);
-  $(this).toggleClass('active');
-  if (!$(this).hasClass('active')) {
-    $(this).find('.button-animated-text').animate({
-      textAlign: 'center',
-      flexGrow: 1,
-    });
-  } else {
-    $(this).find('.button-animated-text').animate({
-      textAlign: 'left',
-      flexGrow: 0,
-    });
-  }
-});
-
-$('.levels__list-item').each(function(index) {
-    $(this).click(function() {
-    levelSwiper.slideTo(index);
-    $('.levels__list-item').removeClass('active');
-    $(this).addClass('active');
-    sliderAnimate(index)
-  });
-});
